78eda87606a29e54c61810d326763a9ef10d1752
lucy <lucykaufmann@protonmail.com>
2025-12-25 15:11:25 +0100

    push before refactoring to use mini.nvi

--- init.lua
+++ init.lua
@@ -1,6 +1,7 @@
-vim.cmd.colorscheme("wallust")
 require("config.lazy")
 require("core.options")
 require("core.keymaps")
 require("mason-lspconfig").setup()
 require("telescope").load_extension("file_browser")
+vim.cmd.colorscheme("wallust")
+vim.opt.termguicolors = true
