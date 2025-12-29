2d228bde3589d0b6bf2c95f7b253a2a5f038e465
lucy <lucykaufmann@protonmail.com>
2025-12-29 20:06:23 +0100

    initial commit - starting from scratch

--- init.lua
+++
@@ -1,7 +0,0 @@
-require("config.lazy")
-require("core.options")
-require("core.keymaps")
-require("mason-lspconfig").setup()
-require("telescope").load_extension("file_browser")
-vim.cmd.colorscheme("wallust")
-vim.opt.termguicolors = true
--- lua/plugins/neocord.lua
+++
@@ -1,9 +0,0 @@
-return {
-	{
-		"vyfor/cord.nvim",
-		build = ":Cord update",
-		opts = {
-			editor = { tooltip = "if u read this, ur gay" },
-		},
-	},
-}
