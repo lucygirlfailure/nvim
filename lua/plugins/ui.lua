0f6eb1013d517700fa8abf124e7194478d88af15
lucy <lucykaufmann@protonmail.com>
2026-01-21 15:49:05 +0100

    add bufferline and utility keybinds

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -1,17 +1,17 @@
 return {
 	{
-		'nvim-telescope/telescope.nvim',
-		version = '*',
+		"nvim-telescope/telescope.nvim",
+		version = "*",
 		dependencies = {
-			'nvim-lua/plenary.nvim',
+			"nvim-lua/plenary.nvim",
 		},
 		config = function()
 			-- default telescope bindings
-			local builtin = require('telescope.builtin')
-			vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
-			vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
-			vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
-			vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
+			local builtin = require("telescope.builtin")
+			vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Telescope find files" })
+			vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "Telescope live grep" })
+			vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "Telescope buffers" })
+			vim.keymap.set("n", "<leader>fh", builtin.help_tags, { desc = "Telescope help tags" })
 		end,
 	},
 	{
@@ -36,6 +36,14 @@ return {
 					lualine_z = { "location" },
 				},
 			})
+		end,
+	},
+	{
+		"akinsho/bufferline.nvim",
+		version = "*",
+		dependencies = "nvim-tree/nvim-web-devicons",
+		opts = function()
+			require("bufferline").setup()
 		end,
 	},
 }
