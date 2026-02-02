b64c03566b78d92cdabef81d676955cd78ca5709
lucy <lucykaufmann@protonmail.com>
2026-02-02 20:25:40 +0100

    add harpoon bindings

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,20 +1,20 @@
 local map = vim.keymap.set
 vim.g.mapleader = " "
 -- lsp keybinds
-map("n", "k", "<cmd> lspsaga hover_doc<cr>")
+map("n", "K", "<cmd> Lspsaga hover_doc<cr>")
 map("n", "go", "<cmd> lspsaga goto_definition<cr>")
 map("n", "gr", "<cmd> lspsaga finder<cr>")
 map("n", "<leader>qf", "<cmd> lspsaga code_action<cr>")
 -- bufferline keybinds
-map("n", "<a-1>", "<cmd>:lua require('harpoon.ui').nav_file(1) <cr>")
-map("n", "<A-2>", "<cmd>:lua require('harpoon.ui').nav_file(2) <cr>")
-map("n", "<A-3>", "<cmd>:lua require('harpoon.ui').nav_file(3) <cr>")
-map("n", "<A-4>", "<cmd>:lua require('harpoon.ui').nav_file(4) <cr>")
-map("n", "<A-5>", "<cmd>:lua require('harpoon.ui').nav_file(5) <cr>")
-map("n", "<A-6>", "<cmd>:lua require('harpoon.ui').nav_file(6) <cr>")
-map("n", "<A-7>", "<cmd>:lua require('harpoon.ui').nav_file(7) <cr>")
-map("n", "<A-8>", "<cmd>:lua require('harpoon.ui').nav_file(8) <cr>")
-map("n", "<A-9>", "<cmd>:lua require('harpoon.ui').nav_file(9) <cr>")
+map("n", "<a-1>", "<cmd>lua require('harpoon.ui').nav_file(1) <cr>")
+map("n", "<A-2>", "<cmd>lua require('harpoon.ui').nav_file(2) <cr>")
+map("n", "<A-3>", "<cmd>lua require('harpoon.ui').nav_file(3) <cr>")
+map("n", "<A-4>", "<cmd>lua require('harpoon.ui').nav_file(4) <cr>")
+map("n", "<A-5>", "<cmd>lua require('harpoon.ui').nav_file(5) <cr>")
+map("n", "<A-6>", "<cmd>lua require('harpoon.ui').nav_file(6) <cr>")
+map("n", "<A-7>", "<cmd>lua require('harpoon.ui').nav_file(7) <cr>")
+map("n", "<A-8>", "<cmd>lua require('harpoon.ui').nav_file(8) <cr>")
+map("n", "<A-9>", "<cmd>lua require('harpoon.ui').nav_file(9) <cr>")
 -- util keybinds
 map("n", "<leader>t", "<cmd> terminal <cr>")
 map("t", "<esc>", "<c-\\><c-n>")
